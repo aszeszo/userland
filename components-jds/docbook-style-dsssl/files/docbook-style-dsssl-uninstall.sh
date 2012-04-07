6308aeeb33df95827be1521f302b58918457cf6d
Andrzej Szeszo <aszeszo@gmail.com>
2012-04-07 18:31:37 +0100

    Add docbook-style-dsssl package

---
+++ components-jds/docbook-style-dsssl/files/docbook-style-dsssl-uninstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# borrowed from Fedora spec file:
+# http://pkgs.fedoraproject.org/gitweb/?p=docbook-style-dsssl.git;a=blob;f=docbook-style-dsssl.spec
+
+version=1.79
+
+for centralized in /etc/sgml/*-docbook-*.cat
+  do
+    /usr/bin/install-catalog --remove $centralized \
+    /usr/share/sgml/docbook/dsssl-stylesheets-${version}/catalog \
+    > /dev/null 2>/dev/null
+done
